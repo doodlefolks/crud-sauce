599af7849b6966bdb53e4ae5042c1022f9f3199d
Andrew Lowe <doodlefolks@gmail.com>
2016-04-18 14:56:01 -0700

    Fix neighborhoods migrations

--- migrations/20160418132506_neighborhoods.js
+++ migrations/20160418132506_neighborhoods.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('neighborhoods', (table) => {
     table.increments();
+    table.string('name');
     table.decimal('lat', 10, 7);
     table.decimal('long', 10, 7);
   });
