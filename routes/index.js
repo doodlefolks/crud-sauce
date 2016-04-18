213c12e7923ee2529e627e2a3cf8cfb6b76af3de
Edward Burgess <elburgess1@gmail.com>
2016-04-18 16:36:59 -0700

    add db and test code

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  function spots() {
+    return knex('places').select('address');
+  }
+  spots().then(function(address){
+    console.log(encodeURIComponent(address[0].address));
+  });
   res.render('index', { title: 'Express' });
 });
 
