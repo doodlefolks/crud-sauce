a6231640ee756b8d99812f0281f5deff276e0c13
Andrew Lowe <doodlefolks@gmail.com>
2016-04-20 09:35:10 -0700

    Add fb photo to page when logged in

--- passport.js
+++ passport.js
@@ -2,15 +2,17 @@ var passport = require('passport');
 var users = require('./models/users');
 
 passport.authCallback = function (accessToken, refreshToken, profile, done) {
+  // console.log(profile);
+  console.log(accessToken);
   users.getByFacebookId(profile.id)
   .then(function (user) {
     if (!user[0]) {
       users.addUser(profile)
       .then(function () {
-        done(null, {id: profile.id, displayName: profile.displayName});
+        done(null, {id: profile.id, displayName: profile.displayName, accessToken: accessToken});
       });
     } else {
-      done(null, {id: profile.id, displayName: profile.displayName});
+      done(null, {id: profile.id, displayName: profile.displayName, accessToken: accessToken});
     }
   });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var rp = require('request-promise');
 var neighborhoodsModel = require('../models/neighborhoods');
 var placesModel = require('../models/places');
 
 router.get('/', function(req, res, next) {
-  Promise.all([neighborhoodsModel.getAllHoods(), placesModel.getAllPlaces()])
+  var fbInfo = rp({
+    uri: `https://graph.facebook.com/me?access_token=${res.locals.user.accessToken}&fields=picture.type(small)`,
+    json: true,
+  }).catch(function (err) {
+    console.log(err);
+  });
+
+  Promise.all([neighborhoodsModel.getAllHoods(), placesModel.getAllPlaces(), fbInfo])
   .then( data => {
     var hoods = data[0];
     var places = data[1];
+    res.locals.user.picUrl = data[2].picture.data.url
     res.locals.pageData = {};
     hoods.forEach( hood => {
       res.locals.pageData[hood.name] = new Array();
