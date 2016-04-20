a75703724ed0a877820bda4f080f48c911620dcb
Andrew Lowe <doodlefolks@gmail.com>
2016-04-20 10:43:30 -0700

    Fix fb prof pic when not signed in

--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,29 @@ var neighborhoodsModel = require('../models/neighborhoods');
 var placesModel = require('../models/places');
 
 router.get('/', function(req, res, next) {
-  // var fbInfo = rp({
-  //   uri: `https://graph.facebook.com/me?access_token=${res.locals.user.accessToken}&fields=picture.type(small)`,
-  //   json: true,
-  // }).catch(function (err) {
-  //   console.log(err);
-  // });
-
-  Promise.all([neighborhoodsModel.getAllHoods(), placesModel.getAllPlaces()])
-  .then( data => {
-    var hoods = data[0];
-    var places = data[1];
-    // res.locals.user.picUrl = data[2].picture.data.url
-    res.locals.pageData = {};
-    hoods.forEach( hood => {
-      res.locals.pageData[hood.name] = new Array();
-      places.forEach( place => {
-        if (hood.id === place.neighborhood_id) {
-          res.locals.pageData[hood.name].push(place);
-        }
+  rp({
+    uri: `https://graph.facebook.com/me?access_token=${res.locals.user.accessToken}&fields=picture.type(small)`,
+    json: true,
+  }).catch(function (err) {
+    res.locals.user = null;
+  }).then(function (fbInfo) {
+    Promise.all([neighborhoodsModel.getAllHoods(), placesModel.getAllPlaces()])
+    .then( data => {
+      var hoods = data[0];
+      var places = data[1];
+      var user = res.locals.user;
+      if (user) res.locals.user.picUrl = fbInfo.picture.data.url;
+      res.locals.pageData = {};
+      hoods.forEach( hood => {
+        res.locals.pageData[hood.name] = new Array();
+        places.forEach( place => {
+          if (hood.id === place.neighborhood_id) {
+            res.locals.pageData[hood.name].push(place);
+          }
+        });
       });
+      res.render('index');
     });
-    res.render('index');
   });
 });
 
