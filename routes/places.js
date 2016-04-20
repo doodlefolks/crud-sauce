c82ab9d561254922b94b212d2fec7f383440a3d0
Andrew Lowe <doodlefolks@gmail.com>
2016-04-20 13:34:06 -0700

    Add user comment submission

--- routes/places.js
+++ routes/places.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var places = require('../models/places');
+var reviews = require('../models/reviews');
+var users = require('../models/users');
 
-router.get('/:index', function(req, res, next) {
+router.get('/:index', function (req, res, next) {
   places.getPlaceById(req.params.index)
   .then(function (place) {
     res.locals.place = place[0];
     res.render('places/index');
+  });
+});
+
+router.post('/:index', function (req, res, next) {
+  // res.send(res.locals.user.id);
+  users.getByFacebookId(res.locals.user.id).then(function (user) {
+    reviews.addReview(user[0].user_id, req.params.index, req.body.comment, req.body.rating).then(function () {
+      places.getPlaceById(req.params.index).then(function (place) {
+        res.locals.place = place[0];
+        res.render('places/index');
+      });
+    }).catch(function (err) {
+      console.log(err);
+    });
   });
 });
 
