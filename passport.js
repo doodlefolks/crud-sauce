037d8bee163b60f0708bf9551d5c7109dc332e9b
Andrew Lowe <doodlefolks@gmail.com>
2016-04-21 08:50:53 -0700

    Add prof pic logic to login sequence

--- passport.js
+++ passport.js
@@ -1,18 +1,25 @@
 var passport = require('passport');
 var users = require('./models/users');
+var rp = require('request-promise');
 
 passport.authCallback = function (accessToken, refreshToken, profile, done) {
-  // console.log(profile);
-  console.log(accessToken);
   users.getByFacebookId(profile.id)
   .then(function (user) {
     if (!user[0]) {
-      users.addUser(profile)
-      .then(function () {
-        done(null, {id: profile.id, displayName: profile.displayName, accessToken: accessToken});
+      rp({
+        uri: `https://graph.facebook.com/me?access_token=${accessToken}&fields=picture.type(small)`,
+        json: true,
+      }).catch(function () {
+        profile.picUrl = null;
+      }).then(function (fbInfo) {
+        profile.picUrl = fbInfo.picture.data.url;
+        users.addUser(profile)
+        .then(function () {
+          done(null, {id: profile.id, displayName: profile.displayName, accessToken: accessToken, picUrl: profile.picUrl});
+        });
       });
     } else {
-      done(null, {id: profile.id, displayName: profile.displayName, accessToken: accessToken});
+      done(null, {id: profile.id, displayName: profile.displayName, accessToken: accessToken, picUrl: profile.picUrl});
     }
   });
 };
